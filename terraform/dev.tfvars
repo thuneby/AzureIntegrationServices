98d421b7beda2021854f86db5147f5dd105e7219
John Thuneby <thuneby@yahoo.dk>
2024-03-24 21:00:28 +0100

    Moved variables to tfvars

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -1,1 +1,4 @@
-apim_sku = Consumption_0
+apim_sku       = Consumption_0
+analytics_sku  = PerGB2018
+servicebus_sku = Basic
+key_vault_sku  = standard
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,27 +8,3 @@ variable "env" {
   description = "The environment. dev, test or prod."
 }
 
-# variable "apim_sku" {
-#   type    = string
-#   default = "Consumption_0"
-# }
-
-variable "analytics_sku" {
-  type    = string
-  default = "PerGB2018"
-}
-
-variable "servicebus_sku" {
-  type    = string
-  default = "Basic"
-}
-
-variable "key_vault_sku" {
-  type    = string
-  default = "standard"
-}
-
-variable "app_configuration_sku" {
-  type    = string
-  default = "free"
-}

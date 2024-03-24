0245670ca3162be724653ca67ef2e43d6b3add40
John Thuneby <thuneby@yahoo.dk>
2024-03-24 12:34:19 +0100

    update workflow

--- terraform/resource_analytics.tf
+++ terraform/resource_analytics.tf
@@ -1,16 +1,16 @@
-locals {
-  analytics_name = "platform"
-}
+# locals {
+#   analytics_name = "integrations"
+# }
 
-resource "azurecaf_name" "rg-analytics-name" {
-  name          = "analytics"
-  resource_type = "azurerm_resource_group"
-}
+# resource "azurecaf_name" "rg-analytics-name" {
+#   name          = "analytics"
+#   resource_type = "azurerm_resource_group"
+# }
 
-resource "azurerm_log_analytics_workspace" "analytics" {
-  name                = local.analytics_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg_shared_services.name
-  sku                 = var.analytics_sku
-  retention_in_days   = 30
-}
+# resource "azurerm_log_analytics_workspace" "analytics" {
+#   name                = local.analytics_name
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg_shared_services.name
+#   sku                 = var.analytics_sku
+#   retention_in_days   = 30
+# }

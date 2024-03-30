1a700893cdb57836f2a69a979e08070b8f130cec
John Thuneby <thuneby@yahoo.dk>
2024-03-30 17:39:11 +0100

    Added event db

--- terraform/resource_cosmos.tf
+++ terraform/resource_cosmos.tf
@@ -1,7 +1,8 @@
 locals {
-  cosmos_account_name = "integrations-mycorp"
-  cosmos_db_name      = "integrations-${var.env}"
-  container_name      = "products"
+  cosmos_account_name    = "integrations-mycorp"
+  cosmos_db_name         = "integrations-${var.env}"
+  product_container_name = "products"
+  event_container_name   = "events"
 }
 
 resource "azurecaf_name" "cosmos_account_name" {
@@ -44,7 +45,16 @@ resource "azurerm_cosmosdb_sql_database" "main" {
 }
 
 resource "azurerm_cosmosdb_sql_container" "products" {
-  name                  = local.container_name
+  name                  = local.product_container_name
+  resource_group_name   = azurerm_resource_group.rg_shared_services.name
+  account_name          = azurerm_cosmosdb_account.integrations.name
+  database_name         = azurerm_cosmosdb_sql_database.main.name
+  partition_key_path    = "/id"
+  partition_key_version = 1
+}
+
+resource "azurerm_cosmosdb_sql_container" "events" {
+  name                  = local.event_container_name
   resource_group_name   = azurerm_resource_group.rg_shared_services.name
   account_name          = azurerm_cosmosdb_account.integrations.name
   database_name         = azurerm_cosmosdb_sql_database.main.name

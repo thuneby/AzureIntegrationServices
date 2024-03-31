cb62f5c2cad189b3dbbcc05d48df7b62e1a38bba
John Thuneby <thuneby@yahoo.dk>
2024-03-31 18:14:29 +0200

    fix trigger

--- terraform/resource_workflow.tf
+++ terraform/resource_workflow.tf
@@ -1,5 +1,6 @@
 locals {
   workflow_name = "workflow1"
+  schema        = jsonencode((file("${path.module}/cloudEventSpec.json")))
 }
 
 resource "azurecaf_name" "workflow_name" {
@@ -15,15 +16,26 @@ resource "azurerm_logic_app_workflow" "workflow1" {
   resource_group_name = azurerm_resource_group.rg_shared_services.name
 }
 
+resource "azurerm_logic_app_trigger_http_request" "workflow1_trigger" {
+  name         = "workflow1-trigger"
+  logic_app_id = azurerm_logic_app_workflow.workflow1.id
+  schema       = <<SCHEMA
+    ${local.schema}
+    SCHEMA
+}
+
 resource "azurerm_eventgrid_system_topic_event_subscription" "fileuploaded_flow" {
-  name                  = "fileuploaded-workflow"
-  system_topic          = azurerm_eventgrid_system_topic.fileupload.name
-  resource_group_name   = azurerm_resource_group.rg_shared_services.name
+  name                = "fileuploaded-workflow"
+  system_topic        = azurerm_eventgrid_system_topic.fileupload.name
+  resource_group_name = azurerm_resource_group.rg_shared_services.name
+  webhook_endpoint {
+    url = azurerm_logic_app_trigger_http_request.workflow1_trigger.callback_url
+  }
   event_delivery_schema = "CloudEventSchemaV1_0"
   included_event_types  = ["Microsoft.Storage.BlobCreated"]
 
   depends_on = [
     azurerm_eventgrid_system_topic.fileupload,
-    azurerm_logic_app_workflow.workflow1
+    azurerm_logic_app_trigger_http_request.workflow1_trigger
   ]
 }
